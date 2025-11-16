f779a35f441d05fc5316bdd914e9e39f48b3fc72
danielmhi111 <danielmartinhill@gmail.com>
2025-11-16 16:52:34 +0000

    parse coords from geojson

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import json
+from pathlib import Path
+
+from project_paths import paths
+from rich.pretty import pprint
+
+LOCATIONS = paths.locations
+
+streetlevel_base_url = "https://data.police.uk/api/crimes-street/all-crime?"
+
+
+def get_coords(polygon_file: Path) -> str:
+    with open(polygon_file) as file:
+        polygon = json.load(file)
+
+    coords = polygon["coordinates"][0]
+    formatted_coords = ":".join([str(lon) + "," + str(lat) for lon, lat in coords])
+
+    return formatted_coords
+
+
 def main():
-    print("Hello from policedatascraper!")
+    constituencies = [
+        "bristol_east",
+        "bristol_north",
+        "bristol_south",
+        "bristol_west",
+    ]
+
+    constituency_coords = {
+        key: get_coords(LOCATIONS / (key + ".geojson")) for key in constituencies
+    }
+
+    pprint(constituency_coords)
 
 
 if __name__ == "__main__":
