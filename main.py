c5d13e0ca04d3f277b61aa9ec5e7ab5d5af8d3c7
danielmhi111 <danielmartinhill@gmail.com>
2025-11-17 00:01:28 +0000

    add months to request params and format responses into a dataframe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+import time
+from itertools import product
 from pathlib import Path
 
+import polars as pl
 import requests
 from loguru import logger
 from project_paths import paths
@@ -8,7 +11,15 @@ from rich.pretty import pprint
 
 LOCATIONS = paths.locations
 
-STREETLEVEL_BASE_URL = "https://data.police.uk/api/crimes-street/all-crime?"
+STREETLEVEL_BASE_URL = "https://data.police.uk/api/crimes-street/all-crime"
+
+
+def generate_months() -> list[str]:
+    dates = [
+        str(year) + "-" + str(month).rjust(2, "0")
+        for year, month in product(range(2022, 2026), range(1, 13))
+    ]
+    return dates
 
 
 def get_coords(polygon_file: Path) -> str:
@@ -31,23 +42,37 @@ def get_coords(polygon_file: Path) -> str:
     return formatted_coords
 
 
-def construct_url(location_names: list[str]) -> dict[str, str]:
-    location_urls = {
-        key: STREETLEVEL_BASE_URL + "poly=" + get_coords(LOCATIONS / (key + ".geojson"))
-        for key in location_names
-    }
+def construct_url(location_names: list[str], dates: list[str]) -> list[tuple[str, str]]:
+    params = product(dates, location_names)
+
+    location_urls = [
+        (
+            location_name,
+            STREETLEVEL_BASE_URL
+            + "?date="
+            + month
+            + "&poly="
+            + get_coords(LOCATIONS / (location_name + ".geojson")),
+        )
+        for month, location_name in params
+    ]
     return location_urls
 
 
-def make_request(url: str) -> list[dict]:
-    response = requests.get(url=url)
+def make_request(url: str) -> list[dict] | None:
+    try:
+        response = requests.get(url=url)
+    except requests.HTTPError:
+        return None
+    finally:
+        time.sleep(0.25)
 
-    print(f"status code: {response.status_code}")
+    logger.debug(f"status code: {response.status_code}")
 
     if response.status_code == 200:
         return response.json()
-    else:
-        raise requests.HTTPError
+    elif response.status_code == 404:
+        logger.warning(f"404 status returned for {url}")
 
 
 def find_lsoas() -> list[str]:
@@ -56,13 +81,38 @@ def find_lsoas() -> list[str]:
     return names
 
 
+def format_data(data: list[dict[str, list[dict]]]) -> pl.DataFrame:
+    flat_data = [
+        {
+            **crime,
+            "lsoa": lsoa,
+            "status": (crime.get("outcome_status") or {}).get("category"),
+        }
+        for report in data
+        for lsoa, crimes in report.items()
+        for crime in crimes
+        if crimes != []
+    ]
+
+    pprint(flat_data)
+
+    crimes_df = pl.DataFrame(flat_data)
+    crimes_df = crimes_df.select(["lsoa", "month", "category", "status"])
+    return crimes_df
+
+
 def main():
     lsoas = [find_lsoas()[0]]
-    lsoa_urls = construct_url(lsoas)
-    data = {constituency: make_request(url) for constituency, url in lsoa_urls.items()}
+    lsoa_urls = construct_url(lsoas, generate_months())
+    data: list[dict[str, list]] = [
+        {lsoa: (make_request(url) or [])} for lsoa, url in lsoa_urls
+    ]
+    pprint(data)
 
-    if data:
-        pprint(data)
+    crimes_df = format_data(data)
+    print(crimes_df)
+
+    print(crimes_df.select("month").n_unique())
 
 
 if __name__ == "__main__":
